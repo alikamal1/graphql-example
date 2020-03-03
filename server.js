9eff832c94729d1b2105e0e0597c5041de0df6bb
Ali Kamal <alikamal1@yahoo.com>
2020-03-03 15:10:28 +0300

    scema

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const  {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+const app = express()
+
+const authors = [
+    {id: 1, name: 'J. K. Rowling'},
+    {id: 2, name: 'J. R. R. Tolkien'},
+    {id: 3, name: 'Brent Weeks'},
+]
+
+const books = [
+    {id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1},
+    {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
+    {id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1},
+    {id: 4, name: 'The Fellowship if the Ring', authorId: 2},
+    {id: 5, name: 'The Two Towers', authorId: 2},
+    {id: 6, name: 'The Returns of the King', authorId: 2},
+    {id: 7, name: 'The way of Shadows', authorId: 3},
+    {id: 8, name: 'Beyond the Shadows', authorId: 3},
+]
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello world'
+            }
+        })
+    })
+})
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true,
+    schema: schema
+}))
+app.listen(5000, () => console.log('Server is Running!'))
