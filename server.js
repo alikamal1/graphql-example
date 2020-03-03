c89b71315ab809399c9cccdac171d4834f981cfc
Ali Kamal <alikamal1@yahoo.com>
2020-03-03 15:25:17 +0300

    books

--- server.js
+++ server.js
@@ -1,39 +1,56 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
-const  {
+const {
     GraphQLSchema,
     GraphQLObjectType,
-    GraphQLString
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
 } = require('graphql')
 const app = express()
 
 const authors = [
-    {id: 1, name: 'J. K. Rowling'},
-    {id: 2, name: 'J. R. R. Tolkien'},
-    {id: 3, name: 'Brent Weeks'},
+    { id: 1, name: 'J. K. Rowling' },
+    { id: 2, name: 'J. R. R. Tolkien' },
+    { id: 3, name: 'Brent Weeks' },
 ]
 
 const books = [
-    {id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1},
-    {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1},
-    {id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1},
-    {id: 4, name: 'The Fellowship if the Ring', authorId: 2},
-    {id: 5, name: 'The Two Towers', authorId: 2},
-    {id: 6, name: 'The Returns of the King', authorId: 2},
-    {id: 7, name: 'The way of Shadows', authorId: 3},
-    {id: 8, name: 'Beyond the Shadows', authorId: 3},
+    { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+    { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+    { id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+    { id: 4, name: 'The Fellowship if the Ring', authorId: 2 },
+    { id: 5, name: 'The Two Towers', authorId: 2 },
+    { id: 6, name: 'The Returns of the King', authorId: 2 },
+    { id: 7, name: 'The way of Shadows', authorId: 3 },
+    { id: 8, name: 'Beyond the Shadows', authorId: 3 },
 ]
 
-const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name: 'HelloWorld',
-        fields: () => ({
-            message: {
-                type: GraphQLString,
-                resolve: () => 'Hello world'
-            }
-        })
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'this represent a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt)},
+        name: { type: GraphQLNonNull(GraphQLString)},
+        authorId: { type: GraphQLNonNull(GraphQLInt)},
     })
+})
+
+const RooQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of all Books',
+            resolve: () => books
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RooQueryType
 })
 
 app.use('/graphql', expressGraphQL({
